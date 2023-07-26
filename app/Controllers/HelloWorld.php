58e257d0314e64fab5eea05438a16d25dd3182ee
unknown <senopati@std.unissula.ac.id>
2023-07-27 03:37:52 +0700

    first commit

---
+++ app/Controllers/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\controllers;
+
+class HelloWorld extends BaseController
+{
+    public function index() {
+        return view ('halloworld');
+    }
+}
+?>
---
+++ app/Controllers/LayoutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class LayoutController extends BaseController
+{
+    public function index() 
+    {
+        return view ('ndaru_32602100104');
+    }
+}
+?>
